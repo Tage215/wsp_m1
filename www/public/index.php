2b5801aa002a82a5ca299ee12101b04f0d43a24a
Tage <05jota@skola.engelholm.se>
2023-10-18 13:34:30 +0200

    a

--- www/public/index.php
+++ www/public/index.php
@@ -8,35 +8,6 @@
 </head>
 
 <body>
-   <?php
-   for ($i = 1; $i <= 5; $i += 0.1) {
-      print("$i ");
-   }
-
-   print "<br>";
-
-   $index = 1;
-   while ($index <= 5) {
-      print("$index ");
-      $index += 0.1;
-   }
-
-   $page["head"] = "<h1>Välkommen</h1>";
-   $page["main"] = "<p>Detta är innehållet på min sida</p>";
-   $page["footer"] = "<hr><p>Min sidfoot</p>";
-   foreach ($page as $value) {
-      print("$value <br>");
-   }
-
-   include('math.php');
-   print(sum(10, 5) . "<br>");
-   print(multiply(10, 5) . "<br>");
-   print(subtract(10, 5) . "<br>");
-   print(divide(10, 5) . "<br>");
-
-   
-   ?>
-
    <form action="user.php" method="post">
       <fieldset>
          <legend>Användare</legend>
@@ -76,6 +47,32 @@
    </form>
 
    <?php
+   for ($i = 1; $i <= 5; $i += 0.1) {
+      print("$i ");
+   }
+
+   print "<br>";
+
+   $index = 1;
+   while ($index <= 5) {
+      print("$index ");
+      $index += 0.1;
+   }
+
+   $page["head"] = "<h1>Välkommen</h1>";
+   $page["main"] = "<p>Detta är innehållet på min sida</p>";
+   $page["footer"] = "<hr><p>Min sidfoot</p>";
+   foreach ($page as $value) {
+      print("$value <br>");
+   }
+
+   include('math.php');
+   print(sum(10, 5) . "<br>");
+   print(multiply(10, 5) . "<br>");
+   print(subtract(10, 5) . "<br>");
+   print(divide(10, 5) . "<br>");
+
+   include("cleanData.php");
    print(cleanData($_POST["namn"]) . "<br>");
    print(cleanData($_POST["efternamn"]) . "<br>");
    print(cleanData($_POST["username"]) . "<br>");
